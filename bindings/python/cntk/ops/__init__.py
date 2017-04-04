1841d9206e01c7d0e7b3a55d22d610f39391d2d2
Mark Hillebrand <mahilleb@microsoft.com>
2017-04-04 10:13:45 +0200

    Tests/EndToEndTests/CNTKv2Python/Sphinx: enable on Windows, add post-Sphinx-build checks

--- bindings/python/cntk/ops/__init__.py
+++ bindings/python/cntk/ops/__init__.py
@@ -2533,6 +2533,8 @@ def _input_spec(shape, dtype=default_override_or(np.float32), needs_gradient=Fal
 def input(shape, dtype=default_override_or(np.float32), needs_gradient=False, is_sparse=False,
           dynamic_axes=[Axis.default_batch_axis()], name=''):
     '''
+    input(shape, dtype=np.float32, needs_gradient=False, is_sparse=False, dynamic_axes=[Axis.default_batch_axis()], name='')
+
     It creates an input in the network: a place where data,
     such as features and labels, should be provided.
 
@@ -2565,7 +2567,8 @@ def input(shape, dtype=default_override_or(np.float32), needs_gradient=False, is
 @typemap
 def input_variable(shape, dtype=np.float32, needs_gradient=False, is_sparse=False,
                    dynamic_axes=Axis.default_input_variable_dynamic_axes(), name=''):
-    '''
+    '''input_variable(shape, dtype=np.float32, needs_gradient=False, is_sparse=False, dynamic_axes=Axis.default_input_variable_dynamic_axes(), name='')
+
     DEPRECATED.
 
     It creates an input in the network: a place where data,
